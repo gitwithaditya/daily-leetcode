add5a4067cde3a673a89dfc072e650d11499efc7
Aditya Upadhyay <upadhyayaditya469@gmail.com>
2026-07-19 23:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (46.04%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+            int n=bills.size();
+        int cnt5=0;
+        int cnt10=0;
+        int cnt20=0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==5) cnt5++;
+            else if(bills[i]==10){
+                if(cnt5) cnt5--;
+                else return 0;
+                cnt10++;
+            }
+            else if(bills[i]==20){
+                if((cnt5 && cnt10)){cnt5--;
+                cnt10--;}
+                else if(cnt5>=3) cnt5-=3;
+                else return 0;
+                cnt20++;
+            }
+        }
+        return 1;
+    }
+};
