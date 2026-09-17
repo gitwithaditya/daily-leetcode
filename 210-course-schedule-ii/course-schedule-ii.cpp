ee6270f4d781cc791eb0f8e2c3f54ce008e4f3f5
Aditya Upadhyay <upadhyayaditya469@gmail.com>
2026-09-17 12:31:09 +0530

    Time: 1 ms (81.02%) | Memory: 18.5 MB (52.57%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,79 +1,60 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        //creating adj list->
-    //     vector<vector<int>> adj(numCourses);
-    //     for(int i=0;i<prerequisites.size();i++){
-    //         int u=prerequisites[i][1];
-    //         int v=prerequisites[i][0];
-    //         adj[u].push_back(v);
-    //     }
+        // USING DFS
 
-    //     stack<int> st;
-    //     vector<int> visited(numCourses,0);
-    //     for(int i=0;i<numCourses;i++){
-    //         if(!visited[i]){
-    //             dfs(i,adj,st,visited);
-    //         }
-    //     }
-    //     vector<int> ans;
-    //     while(!st.empty()){
-    //         ans.push_back(st.top());
-    //         st.pop();
-    //     }
-    //     return ans;
-    // }
+        vector<vector<int>> adj(numCourses);
 
-    // void dfs(int node,vector<vector<int>>& adj,stack<int>& st,vector<int>& visited){
-    //     visited[node]=1;
-    //     for(int i=0;i<adj[node].size();i++){
-    //         if(!visited[adj[node][i]]){
-    //             dfs(adj[node][i],adj,st,visited);
-    //         }
-    //     }
-    //     st.push(node);
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
 
-    //creating adj list->
-    vector<vector<int>> adj(numCourses);
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][1];
-            int v=prerequisites[i][0];
             adj[u].push_back(v);
         }
 
-        //Creating the indegree array of all the nodes
-        vector<int> indegree(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            for(int j=0;j<adj[i].size();j++){
-                indegree[adj[i][j]]++;
-            }
-        }
+        stack<int> st;
+        vector<int> visited(numCourses, 0);
 
-        //Initializing the queue with all the nodes having 0 indegree.
-        vector<int> ans;
-        queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-
-
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            ans.push_back(node);
-            for(int i=0;i<adj[node].size();i++){
-                indegree[adj[node][i]]--;
-                if(indegree[adj[node][i]]==0){
-                    q.push(adj[node][i]);
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (!dfs(i, adj, st, visited)) {
+                    return {};
                 }
             }
         }
-        if (ans.size() != numCourses) {
-            return {};
+
+        vector<int> ans;
+
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
 
         return ans;
+    }
+
+    bool dfs(int node, vector<vector<int>>& adj,
+             stack<int>& st, vector<int>& visited) {
+
+        visited[node] = 1;
+
+        for (int i = 0; i < adj[node].size(); i++) {
+            int neighbour = adj[node][i];
+
+            if (visited[neighbour] == 1) {
+                return false;  // cycle detected
+            }
+
+            if (visited[neighbour] == 0) {
+                if (!dfs(neighbour, adj, st, visited)) {
+                    return false;
+                }
+            }
+        }
+
+        visited[node] = 2;
+        st.push(node);
+
+        return 1;
     }
 };
