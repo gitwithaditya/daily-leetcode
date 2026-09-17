e041e5907d973adfc54640fe35f61721f448d1e5
Aditya Upadhyay <upadhyayaditya469@gmail.com>
2026-09-17 16:56:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (39.43%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> colored(n,-1);
+        for(int j=0;j<n;j++){
+            if(colored[j]==-1){ //unvisited node
+                queue<int>q;
+        q.push(j);
+        colored[j]=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(int i=0;i<graph[node].size();i++){
+                if(colored[graph[node][i]]==-1){
+                    colored[graph[node][i]]=(colored[node]+1)%2;
+                    q.push(graph[node][i]);
+                }
+                else{
+                    if(colored[graph[node][i]]==colored[node]){
+                        return 0;
+                    }
+                }
+            }
+        }
+            }
+        }
+        return 1;
+    }
+};
